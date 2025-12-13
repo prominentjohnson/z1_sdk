5eebed476af7a3cf40541f8739447df73140ae5f
Zhuocheng Zhang <z.zhang@tufast.de>
2025-12-13 17:18:29 +0100

    did some experiments

--- examples/lowcmd_development.cpp
+++ examples/lowcmd_development.cpp
@@ -16,7 +16,7 @@ Vec6 clampVec6(const Vec6& v, double low, double high)
 
 int main(int argc, char *argv[]) {
     using clock = std::chrono::steady_clock;
-    std::ifstream file("../examples/nominal_trajectory.csv");
+    std::ifstream file("../examples/robust_nominal_trajectory.csv");
     if (!file.is_open()) {
         std::cerr << "Error opening file\n";
         return 1;
@@ -122,24 +122,13 @@ int main(int argc, char *argv[]) {
     int catching_steps = t_interp.size();
     Vec6 currentQ;
     Vec6 currentQd;
-    Vec6 desiredQ;
-    Vec6 desiredQd;
-    Vec6 feedforwardTau;
     Vec6 currentTau;
-    std::vector<double> kp = {750, 750, 750, 750, 750, 750};
-    std::vector<double> kd = {50, 50, 50, 50, 50, 50};
-    Eigen::Map<const Vec6> kp_Eigen(kp.data());
-    Eigen::Map<const Vec6> kd_Eigen(kd.data());
-    KP = {0, 0, 0, 0, 0, 0};
-    KW = {0, 0, 0, 0, 0, 0};
-    arm._ctrlComp->lowcmd->setControlGain(KP, KW);
     for(int i(0); i<catching_steps; i++){
-        desiredQ << q_interp.col(i);
-        desiredQd << 0,0,0,0,0,0;//<< q_dot_interp.col(i);
-        feedforwardTau << 0,0,0,0,0,0;//<< tau_interp.col(i);
+        arm.q = q_interp.col(i);
+        arm.qd = q_dot_interp.col(i);
+        outputTau = arm._ctrlComp->armModel->inverseDynamics(arm.q, arm.qd, Vec6::Zero(), Vec6::Zero());//<< tau_interp.col(i);
         currentQ = arm.lowstate->getQ();
         currentQd = arm.lowstate->getQd();
-        outputTau = kp_Eigen.array() * (desiredQ - currentQ).array() + kd_Eigen.array()* (desiredQd - currentQd).array() + feedforwardTau.array();
         arm.tau = clampVec6(outputTau, -tau_limit, tau_limit);
         arm.gripperQ = 0;
 
@@ -147,7 +136,7 @@ int main(int argc, char *argv[]) {
         std::cout << arm.tau.transpose()<<"\n";
         // std::cout << currentTau.transpose() << "and\t"<< arm.tau.transpose()<< "\n";
         // std::cout << currentTau.transpose() << "taucmd:\n"<< arm.tau.transpose()<< "\n";
-        std::cout << desiredQ.transpose()<<"\n" << currentQ.transpose() << "difference:"<< (desiredQ-currentQ).transpose() <<"\n";
+        std::cout << arm.q.transpose()<<"\n" << currentQ.transpose() << "difference:"<< (arm.q-currentQ).transpose() <<"\n";
         
         arm.setArmCmd(arm.q, arm.qd, arm.tau);
         arm.setGripperCmd(arm.gripperQ, arm.gripperW, arm.gripperTau);
@@ -155,43 +144,13 @@ int main(int argc, char *argv[]) {
         timer.sleep();
     }
 
-    // double duration = 1000;
-    // Vec6 targetQ;
-    // Vec6 currentQ;
-    // Vec6 currentQd;
-    // Vec6 currentTau;
-    // Eigen::Map<const Vec6> KP_Eigen(KP.data());
-    // Eigen::Map<const Vec6> KW_Eigen(KW.data());
-    // Vec6 outputTau;
-    // targetQ << q_init;
-    // Timer timer(arm._ctrlComp->dt);
-    // for(int i(0); i<duration; i++){
-    //     arm.q = initQ * (1-i/duration) + targetQ * (i/duration);
-    //     arm.qd = (targetQ - initQ) / (duration * arm._ctrlComp->dt);
-    //     arm.tau = arm._ctrlComp->armModel->inverseDynamics(arm.q, arm.qd, Vec6::Zero(), Vec6::Zero());
-    //     arm.gripperQ = 0;
-    //     currentQ = arm.lowstate->getQ();
-    //     currentQd = arm.lowstate->getQd();
-    //     outputTau = 25.6 * KP_Eigen.array() * (arm.q - currentQ).array() + 0.0128 * KW_Eigen.array()* (arm.qd - currentQd).array() + arm.tau.array();
-    //     currentTau = arm.lowstate->getTau();
-    //     // std::cout << outputTau << "and" << currentTau << "end";
-        
-    //     arm.setArmCmd(arm.q, arm.qd, arm.tau);
-    //     arm.setGripperCmd(arm.gripperQ, arm.gripperW, arm.gripperTau);
-    //     arm.sendRecv();
-    //     timer.sleep();
-    // }
-
-    KP = {20, 30, 30, 20, 15, 10};
-    KW = {2000, 2000, 2000, 2000, 2000, 2000};
-    arm._ctrlComp->lowcmd->setControlGain(KP, KW);
     double holding_time = 2.0;  // seconds
     auto finish_time = clock::now();
     while (std::chrono::duration<double>(clock::now() - finish_time).count() < holding_time) {
         arm.q << q_end;
         arm.qd << 0,0,0,0,0,0;
         outputTau = arm._ctrlComp->armModel->inverseDynamics(arm.q, arm.qd, Vec6::Zero(), Vec6::Zero());
-        arm.tau = clampVec6(outputTau, -30.0, 30.0);
+        arm.tau = clampVec6(outputTau, -tau_limit, tau_limit);
         arm.gripperQ = 0;
         // std::cout << arm.tau.transpose()<<"\n";
         
